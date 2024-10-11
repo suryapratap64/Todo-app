9a572a7596c23a14ea781152f0ed43e6d828e32b
surya pratap <146619684+suryapratap64@users.noreply.github.com>
2024-10-11 21:35:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 //angula and vue are like a tools or framework all thing are present
 //rect dom are convert to code to actual browser dom;
 import App from "./App.jsx";
+import {HashRouter} from 'react-router-dom';
 // import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -11,10 +12,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //react.strictmode are used for finding any error in react is correct this 
 //catch many types of in error in rect
 //ReactDOM.createRoot(document.getElementById("root")).render(
-  ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
   <React.StrictMode>
 
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'))
-;
+  </React.StrictMode>
+  </HashRouter>
+);
+
